7517f41fe7bb3d0131e7193536c4bb3cf8153f01
eax255 <eax@eax.fi>
2018-03-09 22:57:50 +0200

    Add recursive directory traversal and output flags

--- src/90_argparser.cc
+++ src/90_argparser.cc
@@ -7,20 +7,25 @@ static char args_doc[] = "[FILENAME]...";
 static struct argp_option options[] = {
     {.name = "lib", .key = 'L', .arg = "PATH", .flags = 0, .doc = "library path", .group = 0},
     {.name = "single", .key = 's', .arg = 0, .flags = 0, .doc = "single file output", .group = 0},
+    {.name = "recursive", .key = 'r', .arg = 0, .flags = 0, .doc = "recursive directory traverse", .group = 0},
+    {.name = "output", .key = 'o', .arg = "OUTPUT", .flags = 0, .doc = "output file", .group = 0},
     {0}
 };
 struct arguments {
     std::vector<std::string> search_path;
     std::vector<std::string> files;
-    bool single;
-    arguments() : single(false){}
+    bool single,recursive;
+    std::string output;
+    arguments() : single(false) {}
 };
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     arguments *args = (arguments*)state->input;
     switch (key) {
     case 'L': if(arg != NULL) args->search_path.push_back(arg); break;
     case 's': args->single = true; break;
-    case ARGP_KEY_ARG: args->files.push_back(arg);return 0;
+    case 'r': args->recursive = true; break;
+    case 'o': args->output = arg; break;
+    case ARGP_KEY_ARG: args->files.push_back(arg); break;
     default: return ARGP_ERR_UNKNOWN;
     }
     return 0;
--- src/91_segmentmerge.cc
+++ src/91_segmentmerge.cc
@@ -6,12 +6,22 @@ int main(int argc, char* argv[]){
     argp_parse(&argparser, argc, argv, 0, 0, &args);
     parser modules;
     for(auto& it : args.search_path){
-        for(auto& p: fs::directory_iterator(it)){
-            modules.parse_file(p);
+        if(args.recursive){
+            for(auto& p: fs::recursive_directory_iterator(it)){
+                modules.parse_file(p);
+            }
+        }
+        else{
+            for(auto& p: fs::directory_iterator(it)){
+                modules.parse_file(p);
+            }
         }
     }
     for(auto& it : args.files){
        modules.parse_file(it);
     }
-    modules.gen_program(std::cout,{"function","main"},args.single);
+    std::ostream *output = &std::cout;
+    if(args.output.size() != 0) output = new std::ofstream(args.output);
+    modules.gen_program(*output,{"function","main"},args.single);
+    if(args.output.size() != 0) delete output,output=nullptr;
 }
